42da63ff84c87e26c64ed17decb32dda604a439a
mudable <mudableshilpa@gmail.com>
2021-09-13 12:31:05 +0530

    Added:UC5 To check emp wage per month

--- src/empwage/EmployeeWage.java
+++ src/empwage/EmployeeWage.java
@@ -3,34 +3,40 @@ package empwage;
 import java.util.Random;
 
 public class EmployeeWage {
- public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		final int IS_PART_TIME = 1;
-		  final int IS_FULL_TIME = 2;
-		     int EMP_RATE_PER_HOUR = 20;
-	 int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.nextInt(3);
-            switch (empCheck)
-            {
-                case IS_PART_TIME:
-                    empHrs = 8;
-                    System.out.println("Employee is present part time");
-                    break;
+		final int IS_FULL_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
+		int NO_OF_WORKING_DAYS = 20;
 
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    System.out.println("Employee is present full time");
-                    break;
+		int empHrs = 0;
+		int empWage = 0;
+		int totalEmpWage = 0;
+		for (int day = 0; day < NO_OF_WORKING_DAYS; day++) {
+			Random random = new Random();
+			int empCheck = random.nextInt(3);
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
 
-                default:
-                    empHrs = 0;
-                    System.out.println("Employee is absent");
-                    break;
-            }
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
 
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            System.out.println("Emp Wage:" + empWage);
+			default:
+				empHrs = 0;
+				break;
+
+			}
+
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Emp Wage:" + empWage);
+		}
+
+		System.out.println("Total Emp Wage:" + totalEmpWage);
 	}
 
 }
