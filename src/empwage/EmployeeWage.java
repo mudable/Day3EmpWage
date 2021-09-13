a5946dda9b9451d51defd409ac6494c8de5ef16c
mudable <mudableshilpa@gmail.com>
2021-09-13 11:22:12 +0530

    Added:UC2 to calculate daily emp wage

--- src/empwage/EmployeeWage.java
+++ src/empwage/EmployeeWage.java
@@ -4,14 +4,25 @@ import java.util.Random;
 
 public class EmployeeWage {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
-		int IS_FULL_TIME = 1;
+
+		
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
+	
+		int empHrs = 0;
+		int empWage = 0;
 		Random random = new Random();
-		int empCheck = random.nextInt(2);
-		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is present");
-		else
-			System.out.println("Employee is absent");
+		int empCheck = random.nextInt(3);
+		if (empCheck == IS_PART_TIME)
+			empHrs = 4;
+		else if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		// else
+		// empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 
 	}
+
 }
