733144a144efc37c9f8ad1bec9031742bccdf2cd
mudable <mudableshilpa@gmail.com>
2021-09-13 10:58:08 +0530

    Added:UC1_To check emp is present or absent

--- src/empwage/EmployeeWage.java
+++ src/empwage/EmployeeWage.java
@@ -1,7 +1,17 @@
 package empwage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		int IS_FULL_TIME = 1;
+		Random random = new Random();
+		int empCheck = random.nextInt(2);
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+
 	}
 }
